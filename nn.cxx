dd31997d0510ec23917ac3efa6613dde6cb691b4
SuperSiliconosis123 <91914918+SuperSiliconosis123@users.noreply.github.com>
2022-09-11 16:21:48 -0500

    Create nn.cxx

---
+++ nn.cxx
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namspace std;
+
+#define NODE-SIZE 2 //defines the size of every node.
+#define LAYERS 3 //defines the number of layers between the input and output layers
+
+#define INPUT-NODES 10  //defines the number of input nodes. note: nodes are NOT binary. they may have any number of values in the range of their data type
+#define MIDDLE-NODES 30 //defines the number of middle nodes.
+#define OUTPUT-NODES 10 //defines the number of middle nodes.
+
+class net {
+  public:
+    class node {
+      public:
+      
+    class wire {
+      public:
+        
+    };
+};
