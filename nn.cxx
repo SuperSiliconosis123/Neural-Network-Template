d888f1dc8c17e7bc1805176eb3ae94d3d3f3a8ac
SuperSiliconosis123 <91914918+SuperSiliconosis123@users.noreply.github.com>
2022-09-11 20:15:31 -0500

    Update nn.cxx

--- nn.cxx
+++ nn.cxx
@@ -2,17 +2,17 @@
  *                                        A General Template for a Neural Network using the Genetic Algorithm
  * 
  * Settings:
- *   NODE-SIZE:
+ *   NODE_SIZE:
  *     1 = unsigned char
  *     2 = unsigned int
  *     3 = unsigned long long int
  *   LAYERS:
  *     The number of layers of nodes between the input and output layers, aka the hidden layers, non-inclusive.
- *   INPUT-NODES:
+ *   INPUT_NODES:
  *     The number of nodes in the input layer.
- *   MIDDLE-NODES:
+ *   MIDDLE_NODES:
  *     The number of nodes in each of the middle/hidden layers.
- *   OUTPUT-NODES:
+ *   OUTPUT_NODES:
  *     The number of nodes in the output layer.
  *
  */
@@ -22,36 +22,34 @@ using namespace std;
 
 
 //general program settings
-#define NODE-SIZE 2
+#define NODE_SIZE 2
 #define LAYERS 3
 
-#define INPUT-NODES 10
-#define MIDDLE-NODES 30
-#define OUTPUT-NODES 10
+#define INPUT_NODES 10
+#define MIDDLE_NODES 30
+#define OUTPUT_NODES 10
 
 
 //implement the settings
-#if NODE-SIZE == 1
+#if NODE_SIZE == 1
 struct {
-  unsigned char input [INPUT-NODES];
-  unsigned char middle [LAYERS] [MIDDLE-NODES];
-  unsigned char output [OUTPUT-NODES];
+  unsigned char input [INPUT_NODES];
+  unsigned char middle [LAYERS] [MIDDLE_NODES];
+  unsigned char output [OUTPUT_NODES];
 } nodes;
-#elif NODE-SIZE == 2
+#elif NODE_SIZE == 2
 struct {
-  unsigned int input [INPUT-NODES];
-  unsigned int middle [LAYERS] [MIDDLE-NODES];
-  unsigned int output [OUTPUT-NODES];
+  unsigned int input [INPUT_NODES];
+  unsigned int middle [LAYERS] [MIDDLE_NODES];
+  unsigned int output [OUTPUT_NODES];
 } nodes;
-#elif NODE-SIZE == 2
+#elif NODE_SIZE == 2
 struct {
-  unsigned long long int input [INPUT-NODES];
-  unsigned long long int middle [LAYERS] [MIDDLE-NODES];
-  unsigned long long int output [OUTPUT-NODES];
+  unsigned long long int input [INPUT_NODES];
+  unsigned long long int middle [LAYERS] [MIDDLE_NODES];
+  unsigned long long int output [OUTPUT_NODES];
 } nodes;
 #else
-#error NODE-SIZE out of range
+#error "NODE_SIZE out of range"
 #endif
 
-
-//
