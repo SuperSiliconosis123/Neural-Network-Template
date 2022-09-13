427c9b2ae2a0428a2c294c464e72ad327754b6ce
SuperSiliconosis123 <91914918+SuperSiliconosis123@users.noreply.github.com>
2022-09-12 19:07:37 -0500

    Update nn.cxx

--- nn.cxx
+++ nn.cxx
@@ -8,6 +8,8 @@
  *     3 = unsigned long long int
  *   LAYERS:
  *     The number of layers of nodes between the input and output layers, aka the hidden layers, non-inclusive.
+ *   TRIALS:
+ *     The number of trials eahc generation should go through, or rather how many times the parent network should reproduce
  *   INPUT_NODES:
  *     The number of nodes in the input layer.
  *   MIDDLE_NODES:
@@ -24,6 +26,7 @@ using namespace std;
 //general program settings
 #define NODE_SIZE 2
 #define LAYERS 3
+#define TRIALS 3
 
 #define INPUT_NODES 10
 #define MIDDLE_NODES 30
@@ -33,27 +36,21 @@ using namespace std;
 //implement the settings
 #if NODE_SIZE == 1
 struct {
-  unsigned char input1 [INPUT_NODES];
-  unsigned char middle1 [LAYERS] [MIDDLE_NODES];
-  unsigned char output1 [OUTPUT_NODES];
-  unsigned char input2 [INPUT_NODES];
-  unsigned char middle2 [LAYERS] [MIDDLE_NODES];
-  unsigned char output2 [OUTPUT_NODES];
-  unsigned char input3 [INPUT_NODES];
-  unsigned char middle3 [LAYERS] [MIDDLE_NODES];
-  unsigned char output3 [OUTPUT_NODES];
+  unsigned char input [TRIALS] [INPUT_NODES];
+  unsigned char middle [TRIALS] [LAYERS] [MIDDLE_NODES];
+  unsigned char output [TRIALS] [OUTPUT_NODES];
 } nodes;
 #elif NODE_SIZE == 2
 struct {
-  unsigned int input [INPUT_NODES];
-  unsigned int middle [LAYERS] [MIDDLE_NODES];
-  unsigned int output [OUTPUT_NODES];
+  unsigned int input [TRIALS] [INPUT_NODES];
+  unsigned int middle [TRIALS][ [LAYERS] [MIDDLE_NODES];
+  unsigned int output [TRIALS] [OUTPUT_NODES];
 } nodes;
 #elif NODE_SIZE == 2
 struct {
-  unsigned long long int input [INPUT_NODES];
-  unsigned long long int middle [LAYERS] [MIDDLE_NODES];
-  unsigned long long int output [OUTPUT_NODES];
+  unsigned long long int input [TRIALS] [INPUT_NODES];
+  unsigned long long int middle [TRIALS] [LAYERS] [MIDDLE_NODES];
+  unsigned long long int output [TRIALS] [OUTPUT_NODES];
 } nodes;
 #else
 #error "NODE_SIZE out of range"
