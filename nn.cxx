ae749a0c125d03f30f7388420fbcd8a57695b251
SuperSiliconosis123 <91914918+SuperSiliconosis123@users.noreply.github.com>
2022-09-16 07:07:22 -0500

    Update nn.cxx

--- nn.cxx
+++ nn.cxx
@@ -39,29 +39,50 @@ using namespace std;
 
 
 //implement the settings
+#if LAYERS == 0
+#error "LAYERS out of range"
+#endif
 #if NODE_SIZE == 1
 struct {
-  unsigned char input [TRIALS] [INPUT_NODES];
+  unsigned char input [INPUT_NODES];
   unsigned char middle [TRIALS] [LAYERS] [MIDDLE_NODES];
   unsigned char output [TRIALS] [OUTPUT_NODES];
 } nodes;
+struct {
+  unsigned char input [TRIALS] [INPUT_NODES] [MIDDLE_NODES];
+  #if LAYERS > 2
+  unsigned char middle [TRIALS] [LAYERS-2] [MIDDLE_NODES] [MIDDLE_NODES];
+  #endif
+  unsigned char ouTput [TRIALS] [MIDDLE_NODES] [OUTPUT_NODES];
+} wires;
 #elif NODE_SIZE == 2
 struct {
-  unsigned int input [TRIALS] [INPUT_NODES];
+  unsigned int input [INPUT_NODES];
   unsigned int middle [TRIALS] [LAYERS] [MIDDLE_NODES];
   unsigned int output [TRIALS] [OUTPUT_NODES];
 } nodes;
+struct {
+  unsigned int input [TRIALS] [INPUT_NODES] [MIDDLE_NODES];
+  #if LAYERS > 2
+  unsigned int middle [TRIALS] [LAYERS-2] [MIDDLE_NODES] [MIDDLE_NODES];
+  #endif
+  unsigned int ouTput [TRIALS] [MIDDLE_NODES] [OUTPUT_NODES];
+} wires;
 #elif NODE_SIZE == 3
 struct {
-  unsigned long long int input [TRIALS] [INPUT_NODES];
+  unsigned long long int input [INPUT_NODES];
   unsigned long long int middle [TRIALS] [LAYERS] [MIDDLE_NODES];
   unsigned long long int output [TRIALS] [OUTPUT_NODES];
 } nodes;
+struct {
+  unsigned long long int input [TRIALS] [INPUT_NODES] [MIDDLE_NODES];
+  #if LAYERS > 2
+  unsigned long long int middle [TRIALS] [LAYERS-2] [MIDDLE_NODES] [MIDDLE_NODES];
+  #endif
+  unsigned long long int output [TRIALS] [MIDDLE_NODES] [OUTPUT_NODES];
+} wires;
 #else
 #error "NODE_SIZE out of range"
-#endif
-#if LAYERS == 0
-#error "LAYERS out of range"
 #endif
 
 struct {
@@ -70,11 +91,26 @@ struct {
   double middle [TRIALS] [LAYERS-2] [MIDDLE_NODES] [MIDDLE_NODES];
   #endif
   double output [TRIALS] [OUTPUT_NODES] [MIDDLE_NODES];
-} wires;
+} weights;
 
 int evaluate(int trial) {
+  #if NODE_SIZE == 1
+  unsigned char average;
+  #elif NODE_SIZE == 2
+  unsigned int average;
+  #elif NODE_SIZE == 3
+  unsigned long long int average;
+  #enif
   for(int i = 0; i < INPUT_NODES; i++) {
-    for(int x = 0; x < MIDDLE )
+    for(int x = 0; x < MIDDLE_NODES; x++) {
+      wires.input[trial][i][x] = (nodes.input[i] * weights.input[trial][i][x])/INPUT_NODES;
+    }
+  }
+  for(int i = 0; i < MIDDLE_NODES; i++) {
+    for(int x = 0; i < INPUT_NODES; x++) {
+      
+    }
+    node.middle[trial][0][i] = 
   }
   return 0;
 }
